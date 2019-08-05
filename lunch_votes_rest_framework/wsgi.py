579b2963d525f46bbc4eeb3b4f8ec97b3b27f763
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2019-08-05 12:38:31 +0600

    gitignor eupdate

--- lunch_votes_rest_framework/wsgi.py
+++ lunch_votes_rest_framework/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tutorial02 project.
+WSGI config for lunch_votes_rest_framework project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial02.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunch_votes_rest_framework.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial02.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunch_votes_rest_framework.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
